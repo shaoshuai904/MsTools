9d63ac2b4eed1940531fad74751becc9bbd68150
shaoshuai <939078792@qq.com>
2024-06-18 15:05:09 +0800

    update scheme uitls

--- tools/src/main/java/com/maple/tools/scheme/WeiboScheme.kt
+++ tools/src/main/java/com/maple/tools/scheme/WeiboScheme.kt
@@ -8,15 +8,16 @@ import com.maple.tools.base.TitleBean
 object WeiboScheme {
 
     val SCHEME_LIST = mutableListOf<ItemInfoBean>(
-        TitleBean("自定义 Scheme"),
-        NoDataBean("暂无"),
         TitleBean("微博 Scheme"),
         KeyValueBean("首页", "sinaweibo://gotohome"),
         KeyValueBean("超话", "sinaweibo://gotosg"),
-        KeyValueBean("广场", "sinaweibo://discover"),
-        KeyValueBean("消息箱", "sinaweibo://message"),
+        KeyValueBean("视频", "sinaweibo://gotovideo"),
+        KeyValueBean("精选", "sinaweibo://gotofeatured"),
+        KeyValueBean("发现", "sinaweibo://discover"),
+        KeyValueBean("消息", "sinaweibo://message"),
         KeyValueBean("我的", "sinaweibo://myprofile"),
-        KeyValueBean("精选", "sinaweibo://gotofeatured")
+        KeyValueBean("工程模式", "sinaweibo://projectmode"),
+        KeyValueBean("AB", "sinaweibo://greyconfig")
     )
 
 }
